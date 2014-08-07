bc64ddbc732d414fbbcdfe62b54d588ff2caea50
R-Rajkumar <rraju1990@gmail.com>
2014-08-07 11:03:22 +0530

    puppet module tool installer added

---
+++ rr.sh
@@ -0,0 +1,44 @@
+  GNU nano 2.2.6                                                                  File: rr.sh                                                                                                                                          
+
+#!/bin/bash
+ECHO=`which echo`
+function print_usage(){
+    ${ECHO} -e "Puppet Module Tool Installer v1 2014-02-12"
+    ${ECHO} -e "This script will install Puppet Module Tool and download all the puppetforge modules specified in modules.txt"
+    ${ECHO} -e ""
+    ${ECHO} -e "Requirements: "
+    ${ECHO} -e "Puppet Master should be installed before running this script"
+    ${ECHO} -e ""
+    ${ECHO} -e "Usage: "
+    ${ECHO} -e " sh puppetforge"
+    ${ECHO} -e " If you want specific versions of modules, you can specify a version for each modules in moduls.txt"
+    ${ECHO} -e " If you specify versions, dependency modules will not be downloaded automatically, you have to specify all dependency modules and versions in modules.txt"
+    ${ECHO} -e " If you don't specify any versions, puppet module tool will find all the dependencies and download them automatically"
+    ${ECHO} -e ""
+}
+
+function check_for_puppet(){
+# Checking for puppet
+    print_message "Checking for puppet installations ... "
+    if [[ -d '/var/lib/puppet' ]]; then
+print_ok
+    else
+print_error "You should install puppet master before running this script"
+ exit 1
+    fi
+}
+check_for_puppet
+print_usage
+gem install puppet-module
+INPUT=modules.txt
+OLDIFS=$IFS
+IFS=,
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read module version
+do
+        puppet module install $module --version $version
+done < $INPUT
+IFS=$OLDIFS
+
+
+
